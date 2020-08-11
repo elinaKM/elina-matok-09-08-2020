317a2fecb8b69d056d4a36dfe7735ba9342cf6dc
unknown <elina666@gmail.com>
2020-08-11 12:26:02 +0300

    addinig heandy utils

---
+++ src/utils/apiCalls.js
@@ -0,0 +1,16 @@
+import {API_HOST, API_KEY} from './../constants/api'
+
+export const get5DaysWeather = (key) => (
+    fetch(`${API_HOST}forecasts/v1/daily/5day/${key}?metric=true&details=true&apikey=${API_KEY}`)
+    .then(response => response.json())
+)
+
+export const getCurrentConditions = (key) => (
+    fetch(`${API_HOST}currentconditions/v1/${key}?apikey=${API_KEY}`)
+    .then(response => response.json())
+)
+
+export const getAutoComplete = (string) => (
+    fetch(`${API_HOST}locations/v1/cities/autocomplete/${string}?apikey=${API_KEY}`)
+    .then(response => response.json())
+)
