ea115db0f361fd9a2f09bbab0899f2d51ec37161
unknown <elina666@gmail.com>
2020-08-12 22:07:03 +0300

    Updates

---
+++ precache-manifest.940b89315dc5d88dbb3715c346d989fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649458b400b497ebfe3abe02a07497b1",
+    "url": "/elina-matok-09-08-2020/index.html"
+  },
+  {
+    "revision": "dc9951c1e79dfb7c561f",
+    "url": "/elina-matok-09-08-2020/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "344237f20774c74f3423",
+    "url": "/elina-matok-09-08-2020/static/js/2.1f4208b8.chunk.js"
+  },
+  {
+    "revision": "83af3834c661e921ae64d25afaae3aaa",
+    "url": "/elina-matok-09-08-2020/static/js/2.1f4208b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9951c1e79dfb7c561f",
+    "url": "/elina-matok-09-08-2020/static/js/main.0c101259.chunk.js"
+  },
+  {
+    "revision": "2f288c047c9e665127be",
+    "url": "/elina-matok-09-08-2020/static/js/runtime-main.3bb8fa63.js"
+  }
+]);
