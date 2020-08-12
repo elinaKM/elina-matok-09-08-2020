2263b3f6d5e49e102cdffeb3828a683a8fcbd04d
unknown <elina666@gmail.com>
2020-08-12 12:22:44 +0300

    more redux

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,13 +2,14 @@ import autoCompleteActions from './autoCompleteActions'
 import locationActions from './locationActions'
 import fiveDaysForecastActions from './fiveDaysForecastActions'
 import currentWeatherActions from './currentWeatherActions'
+import favoritesActions from './favoritesActions'
 
 const allActions = {
     autoCompleteActions,
     locationActions,
     fiveDaysForecastActions,
-    currentWeatherActions
-    //favoritesAcions
+    currentWeatherActions,
+    favoritesActions
 }
 
 export default allActions
---
+++ src/redux/reducers/favorites.js
@@ -0,0 +1,18 @@
+import { ADD_TO_FAVORITES } from './../../constants/action-types'
+import { REMOVE_FROM_FAVORITES } from './../../constants/action-types'
+
+const favorites = (state = [], action) => {
+  switch (action.type) {
+        case ADD_TO_FAVORITES:
+          return [
+              ...state,
+              action.payload
+          ]
+          case REMOVE_FROM_FAVORITES:
+            return state.filter((item) => item.key!=action.payload.key);
+        default:
+          return state;
+      }
+}
+
+export default favorites
